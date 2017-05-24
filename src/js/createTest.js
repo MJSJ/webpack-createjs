7089b3183bf00a85562dd1d760fc88be3fce8240
yursile <kuangyu02@meituan.com>
2017-05-24 21:25:05 +0800

    add grant

--- src/js/createTest.js
+++ src/js/createTest.js
@@ -1,20 +1,13 @@
 
 /**
  * 给canvas画布定宽高
- * 640*1136
- * 然后利用pageUtil缩放
- * pageUtil是在loading.html中引入的
  */
 var WIDTH  = 640;
 var HEIGHT = 1136;
 let cvsContainer = document.getElementById("cvsContainer");
-// new window.pageUtil("#cvsContainer").response();
+import $ from "jquery"
+import loader from "./load.js";
 
-import "./load.js";
-/**
- * 定义pixi测试类
- * 注意写法
- */
 export default class CreateTest{
 	/**
 	 * 构造函数
@@ -22,20 +15,45 @@ export default class CreateTest{
 	 */
 	constructor(){
 
-        let cvs = cvsContainer.innerHTML = "<canvas id='cvs'></canvas>";
-        this.stage = new createjs.Stage('cvs');
-        var bitmap = new createjs.Bitmap("../img/sohu.png");
-		bitmap.width = 124;
-		bitmap.height = 50;
+        let cvs = cvsContainer.innerHTML = `<canvas id='cvs' width=${WIDTH} height=${HEIGHT}></canvas>`;
+		loader.on("complete",this.initialStage.bind(this))
+        
+	}
+
+	initialStage(){
+		$('.loading').hide();
+		this.stage = new createjs.Stage('cvs');
+
+		
+		this.stage.addChild(this.createSohu());
+
+
+		this.stage.addChild(this.createGrant())
+
+		this.update();
+	}
+	createSohu(){
+		var bitmap = new createjs.Bitmap(loader.getResult("sohu"));
 		bitmap.x = 20;
 		bitmap.y = 20;
-		// bitmap.scaleX = 2;
-		// bitmap.scaleY = 0.5;
-		// console.log(bitmap.getBounds())
-		// bitmap.setBounds(20,20,114,57)
-		// console.log(bitmap.getBounds());
-        this.stage.addChild(bitmap);
-        this.update();
+		return bitmap;
+	}
+
+	createGrant(){
+		let spriteSheet = new createjs.SpriteSheet({
+			framerate: 30,
+			"images": [loader.getResult("grant")],
+			"frames": {"regX": 82, "height": 292, "count": 64, "regY": 0, "width": 165},
+			// define two animations, run (loops, 1.5x speed) and jump (returns to run):
+			"animations": {
+				"run": [0, 25, "run", 1.5],
+				"jump": [26, 63, "run"]
+			}
+		});
+		let grant = new createjs.Sprite(spriteSheet, "run");
+		grant.y = 35;
+		grant.x = 200;
+		return grant;
 	}
 
 
--- src/js/load.js
+++ src/js/load.js
@@ -1,21 +1,17 @@
 var loader = new createjs.LoadQueue();
-loader.on("complete",onComplete);
 
 loader.on("progress",onProgress);
 
-
-loader.loadFile("../img/sohu.png");
-loader.loadFile("../img/sprite.png");
-loader.loadFile("http://news.sohu.com/upload/yursile/dushue/img/shop11.png");
-
-function onComplete(e){
-    setTimeout(()=>{
-        document.querySelector('.loading').style.display="none";
-    },3000);
-}
+loader.loadManifest([
+    { "id": "sohu", "src": "../img/sohu.png" },
+    { "id": "sprite", "src": "../img/sprite.png" },
+    {"id":"smg","src":"http://news.sohu.com/upload/yursile/dushue/img/shop11.png"},
+    { id: "grant",src: "../img/spritesheet_grant.png"}
+]);
 
 function onProgress(e){
     var percent = parseInt(e.progress/e.total * 100) + '%';
     document.querySelector('.pace-progress').innerHTML=percent;
 
 }
+export default loader;
