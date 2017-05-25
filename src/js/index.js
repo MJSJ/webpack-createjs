155792fd9f76ccaf98cfd1cf1a520d0e3247deee
yursile <kuangyu02@meituan.com>
2017-05-25 20:29:04 +0800

    construct

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,18 @@
 import "../css/index.less"
-
 import loader from "./load.js";
 import CreateTest from "./createTest.js"
 
-//初始化createjs 画布
-var test = new CreateTest();
-//两个::,绑定test.initialStage里的this为test
+var main = new CreateTest();
+//两个::,main.initialStage里的this为main
 loader.on("complete",()=>{
 	setTimeout(()=>{
 		$('.loading').hide();
-		test.initialStage()
+		main.initialStage()
 	},2000)
 })
 
-
-/**
- * $jquery是通过CDN载入的，在webpack中配置了externals后，依然可以在这里引入
- */
 import $ from "jquery"
-
-/**
- * 可以加载html
- */
 import template from "../view/testTemplate.html"
-
-/**
- * 加载图片
- * 这个图片小于8k，
- * 会转成base64码
- */
 import vcode from "../img/vcode.jpg"
 // 反引号里可以用${}取js变量
 var style = {
@@ -36,7 +20,6 @@ var style = {
 	top:"50px",
 	right:"0"
 }
-
 
 $("#main").append(template);
 $("#main").append(`<img id='click' src=${vcode} style=position:${style.position};top:${style.top};right:${style.right} />`)
